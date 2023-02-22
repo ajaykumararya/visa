7ac95380292c3581d00190983bca71c7c312316b
Saurabh Kumar Arya <saurabhfzd000@gmail.com>
2023-02-22 16:41:51 +0530

    update

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  
 class Welcome extends CI_Controller { 
 
+	ob_start();
+
 	public function index()
 	{
 		$this->load->view('login');
