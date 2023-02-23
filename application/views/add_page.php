0f47cb2caebe5616475b7a4b5c6653e0243cddce
Saurabh Kumar Arya <saurabhfzd000@gmail.com>
2023-02-23 12:06:19 +0530

    update

--- application/views/add_page.php
+++ application/views/add_page.php
@@ -2,7 +2,7 @@
 include 'include/header.php';
 ?>
 
-
+<div class="row">
 <div class="col-md-6">
           <!-- general form elements -->
           <div class="box box-primary">
@@ -30,6 +30,39 @@ include 'include/header.php';
             </form>
           </div>
 </div>
+
+<div class="col-md-6">
+<div class="card">
+				<div class="card-header bg-primary text-white">
+					<strong><i class="fa fa-list"></i> List Page(s)</strong>
+				</div>
+				<div class="card-body">
+					<div class="table-responsive">
+						<table class="table table-bordered table-striped table-hover">
+							<thead>
+								<tr>
+									<th>#.</th>
+									<th>Name</th>
+									<th>Edit</th>
+									<th>Delete</th>
+									<th>Url</th>
+								</tr>
+							</thead>
+							<!-- <tbody>
+								
+							</tbody> -->
+							
+						</table>
+					</div>
+				</div>
+			</div>
+</div>
+</div>
+
+
+
+
+
 <?php
 include 'include/footer.php';
 ?>
