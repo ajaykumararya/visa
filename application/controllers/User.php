5ab40d22865a67712b41ba03d096f024de2d59bd
Saurabh Kumar Arya <saurabhfzd000@gmail.com>
2023-02-23 14:08:32 +0530

    list page with delete

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -27,9 +27,49 @@ class User extends CI_Controller
 			$this->load->view(__FUNCTION__);
 	}
 
-	public function pages()
+	public function ajax()
 	{
-		$this->load->view(__FUNCTION__);
+		$return = array();
+		if($post = $this->input->post() )
+		{
+			switch ($post['status'])
+			{
+				case 'get_page':
+							$return['html'] = '<table class="table table-striped table-bordered">
+							<thead>
+								<tr>
+									<th>id</th>
+									<th>Name</th>
+									<th>Delete</th>
+								</tr>
+							</thead>
+							<tbody>';
+							$sau = $this->db->get('pages');
+							foreach($sau->result() as $r)
+							{
+
+								$return['html'] .= '<tr>
+								<td>'.$r->id.'</td>
+								<td>'.$r->page_name.'</td>
+								
+								<td class="center"><a href="javascript:delete_page('.$r->id.')" onclick="return confirm(\'Are You Sure?\')"><i class="btn btn-danger zmdi zmdi-delete"><i class="fa fa-trash"></i></i></a>									
+								</td>
+								</tr>'; 
+							}
+							$return['html'] .= '</tbody></table>';
+						break;
+
+						
+
+						case 'del_page':
+							$this->db->where('id',$post["id"])->delete("pages");
+							$return['status'] = '<div class="alert alert-danger"><h4>Delete Page Successfully..</h4></div>';
+							//redirect(site_url('User/all_employee'));
+						break;
+
+			}
+			echo json_encode($return);
+		}
 	}
 }
 ?>
