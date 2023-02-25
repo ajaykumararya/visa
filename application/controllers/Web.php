04d98c21402290fba7df9cbc926332e4bf98c5df
Saurabh Kumar Arya <saurabhfzd000@gmail.com>
2023-02-25 14:30:17 +0530

    2

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -28,21 +28,13 @@ class Web extends CI_Controller
   	}
   	function visa_application_form()
   	{
-		$data = array();
+		$config['upload_path'] = 'upload';
+         $config['allowed_types'] = 'gif|jpg|png|jpeg';
+         $this->load->library('upload', $config);
+         $this->input->post('passport_copy_number');
+         $data_upload_files = $this->upload->data();
 
-  $config = array(
-      'upload_path' => 'upload',
-      'allowed_types' => 'gif|jpg|png|jpeg',
-  );
-  $this->load->library('upload', $config);
-
-  if (!$this->upload->do_upload('passport_copy_upload')) {
-    $error = array('error' => $this->upload->display_errors());
-    // var_dump( $error); die; check errors 
-  } else {
-    $fileName = $this->upload->data();
-    $data['passport_copy_upload'] = $fileName['file_name'];
-  }
+         $passport_copy_number = $data_upload_files['upload'];
 		
 		if($post = $this->input->post()){
 			$data = array(
@@ -66,6 +58,7 @@ class Web extends CI_Controller
 								'start_date_day'=>$post['start_date_day'],
 								'start_date_month'=>$post['start_date_month'],
 								'start_date_year'=>$post['start_date_year'],
+								'passport_copy_upload'=>$post['passport_copy_number'],
 								
 				);
 				$this->db->insert('users',$data);
