5710acc61054c5b213201280d77500c59e7cc949
Saurabh Kumar Arya <saurabhfzd000@gmail.com>
2023-02-27 16:11:03 +0530

    about_us

---
+++ application/views/web/about_us.php
@@ -0,0 +1,46 @@
+<?php include_once "include/header.php";?>
+
+<div class="row body-panel">
+    <div class="wrap pos-Reltv"><a href="https://www.instadubaivisa.com/index.php" class="pageClosebutton"><img
+                src="https://media.instadubaivisa.com/images/delete-icon.png" width="32" height="32" alt="delete icon"
+                title="delete icon"></a>
+        <section class="row">
+            <h1 class="row alignFromCenter font-Weight-Normal titleFont upperCase">About Insta Dubai Visa</h1>
+            <div class="row marginFromTop mobImgfull-width"><img src="https://media.instadubaivisa.com/images/abt.jpg"
+                    width="1170" height="200" title="" alt="About Insta Dubai Visa for UAE Visa Application"
+                    class="borderFromAll box_Sizing"></div>
+            <div class="column-eight container">
+                <h2
+                    class="row alignFromCenter font-Weight-Normal titleFont paddingAll black borderFromBottom marginFromBottom">
+                    To deliver a result driven and customer-centric service by setting a new benchmark in the field of
+                    UAE travel &amp; visa processing</h2>
+                <div class="row alignFromjustify marginFromBottom line-height-reg">
+                    <p class="row marginFromBottom">InstaDubaiVisa.com is dedicated towards providing an inspiring and
+                        premium travel experience. Our services are driven by the vision of uniting smart technology
+                        with customer satisfaction. Apart from specializing in visa services, we facilitate best deals
+                        for our customers in booking hotels and tours in UAE.</p>
+                    <p class="row marginFromBottom">We stand by the three core values across all levels – best available
+                        options, transparent pricing, and premium experience.</p>
+                    <p class="row marginFromBottom">Since our inception, we have processed more than 1 million UAE visas
+                        for our customers from across the world. Our vastly experienced visa processing team carefully
+                        scrutinise your application and related documentation for your UAE visa and ensure that it has
+                        the maximum chances of getting approved by the UAE Government. We provide visa updates at every
+                        stage of the application lifecycle and notify you as soon as your visa is approved.</p>
+                    <p class="row marginFromBottom">We have become the world’s largest supplier of online UAE visas,
+                        successfully processing more than 10,000 UAE visas per month. We are proud of the fact that our
+                        visa rejection rate is less than 0.02%, the absolute lowest in the industry.</p>
+                    <p class="row marginFromBottom">Our focus on technology and security ensures your data remains
+                        encrypted throughout the application processing stage. Our entire website is secured &amp;
+                        protected with Industry-Leading 256-bit SSL.</p>
+                    <p class="row marginFromBottom">Instadubaivisa.com is a product of Proglogix International Services
+                        Pvt Ltd, New Delhi, India.</p>
+                    <div class="container paddingAllExtra"><a href="javascript:void();"
+                            onclick="showApplyNow(&quot;Open&quot;)" class="button-large primary-button">Apply Now</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+
+<?php include_once "include/footer.php";?>
