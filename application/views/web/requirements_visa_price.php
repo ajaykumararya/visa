886c56542214ac91702d247a8a9fe302d5f92023
Saurabh Kumar Arya <saurabhfzd000@gmail.com>
2023-02-28 13:12:48 +0530

    visa price page

--- application/views/web/requirements_visa_price.php
+++ application/views/web/requirements_visa_price.php
@@ -1,22 +1,160 @@
 <?php include_once "include/header.php";?>
 
-<div class="row body-panel padding-0" style="background:#f1f1f1;">
+
+
+
+<style>
+.tabs-menu li a {
+	paddingFrom: 5px 15px;
+}
+.visaDiv {
+	width: 32.33%;
+	border: solid 1px #ddd;
+	border-top-width: 4px;
+	border-bottom-width: 4px;
+	padding: 40px 0px 0px;
+	margin: 10px 0.5%;
+	text-align: center;
+	position:relative;
+}
+.issueDiv {
+	background: #00732f;
+	color: #fff;
+	border-radius: 20px;
+	padding: 5px 5px;
+	margin: -25px auto 20px;
+	width: 78%;
+	float: none;
+}
+.reqBox {
+	padding: 35px;
+	border: solid 1px #d6f6f9;
+	background: #f3feff;
+	border-radius: 500px;
+}
+.navigation {
+	width: 90%;
+	float: none;
+	margin: auto;
+}
+.navigation a {
+	min-width: 10px;
+	padding: 28px 20px;
+}
+.stepDivBox {
+	width: 25%;
+	position: relative;
+	margin-bottom: 30px;
+}
+.stepDivBox:after {
+	content: "";
+	position: absolute;
+	bottom: -40px;
+	width: 20px;
+	margin: auto;
+	height: 20px;
+	right: 0px;
+	left: 0px;
+	background: #fff;
+	border-radius: 100%;
+}
+.stepDivBox:before {
+	content: "";
+	position: absolute;
+	bottom: -33px;
+	width: 248px;
+	margin: auto;
+	height: 5px;
+	left: 48%;
+	background: #666;
+}
+.stepDivBox:last-child:before {
+	width: 0px;
+}
+.pageGapping {
+	padding: 100px 0px;
+}
+.strike {
+	color: #800000;
+	text-decoration: line-through;
+	margin-right: 5px;
+}
+.regImg {
+	border: solid 10px #c6e0e2;
+	border-radius: 100%;
+}
+.how2BG {
+	background: url(../www.ievisa.com/assets/images/dubai-bg.jpg) 0 0/100% 100% fixed no-repeat; border-radius:10px;
+}
+.visaDivHeading{font-size:50px; line-height:42px;}
+.entryType {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    padding: 5px 15px;
+    background: #207ce5;
+    color: #fff;
+    font-size: 14px;
+    right: 0px;
+    margin: auto;
+    width: 140px;
+	text-transform:uppercase;
+}
+.entryType:after{ position:absolute; left:-10px; top:0px; content:""; border-style:solid; border-width:15px 5px; border-color:#207ce5 #207ce5 transparent transparent ;}
+.entryType:before{ position:absolute; right:-10px; top:0px; content:""; border-style:solid; border-width:15px 5px; border-color:#207ce5 transparent transparent #207ce5;}
+@media all and (max-width:767px) {
+.stepDivBox {
+	width: 100%;
+	margin-bottom: 90px;
+}
+.visaDiv {
+	width: 99%;
+}
+.font16 {
+	font-size: 15px!important;
+}
+h4 {
+	font-size: 12px;
+}
+.stepDivBox:before {
+	width: 5px;
+	height: 75px;
+	left: 49.5%;
+	bottom: -100px;
+}
+.reqBox {
+	border-radius: 0px;
+	padding: 10px;
+}
+.pageGapping {
+	padding: 65px 0px;
+}
+.how2BG {
+	background-size: auto 100%;
+}
+.mobWhatsappIcon {
+	margin-right: 10px;
+}
+}
+</style>
+</head>
+
+
+
+  <div class="row body-panel padding-0" style="background:#f1f1f1;">
         
     
     <div class="wrap pos-Reltv pageGapping" id="CVT">
-      <section class="row">
-        <h1 class="row alignFromCenter font-Weight-Normal titleFont upperCase paddingFromBottom marginFromBottom">DUBAI VISA FEES &amp; DOCUMENTS REQUIREMENTS</h1>
+      <section class="row" >
+        <h1 class="row alignFromCenter font-Weight-Normal titleFont upperCase paddingFromBottom marginFromBottom">DUBAI VISA FEES & DOCUMENTS REQUIREMENTS</h1>
         <div class="row">
           <div class="row marginFromBottom paddingAllExtra whiteBG borderFromTop mobpaddingInBox">
             <section class="row">
             <div class="container column-threeForth alignFromCenter font16 fontBold">The only portal with the fastest online Dubai visa application processing view fees , requirement of dubai visa documents online and apply easily.</div>
               <div class="row marginFromBottom paddingFromTop" id="IN">
-                <div class="container column-nine alignFromCenter alert-panel warning font16">Important Note: With Dubai visa, you gain access to all the seven emirates in the United Arab Emirates: Abu Dhabi, Ajman, Dubai, Fujairah, Sharjah, Ras al-Khaimah &amp; Umm al-Quwain.</div>
+                <div class="container column-nine alignFromCenter alert-panel warning font16">Important Note: With Dubai visa, you gain access to all the seven emirates in the United Arab Emirates: Abu Dhabi, Ajman, Dubai, Fujairah, Sharjah, Ras al-Khaimah & Umm al-Quwain.</div>
               </div>
-
-
-
-              <div class="visaDiv"> 
+                            <div class="visaDiv"> 
                 <div class="row fontBold green visaDivHeading">96 Hrs</div>
                 <div class="row fontTwnty marginFromBottom">Transit Visa</div>
                 <div class="entryType">Single Entry</div>
@@ -25,7 +163,7 @@
                   <div class="row font16 label">Stay Validity is 96 hours from the time of entry</div>
                   <div class="row  font16 label marginFromBottom">Visa Validity 60 days from the date of issue</div>
                   <div class="row">
-                   <form id="visaForm" name="visaForm" action="<?=base_url()?>web/visa_application_landing_form" method="post" enctype="multipart/form-data">
+                   <form id="visaForm" name="visaForm" action="https://www.instadubaivisa.com/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
                     <div class="column-five paddingFromRight alignFromRight borderFromRight mob50 mobpaddingInBox">
                       <div class="row red">Regular Visa</div>
                       <div class="row label fontTwnty">$ 149</div>
@@ -34,42 +172,13 @@
                     <div class="column-five paddingFromLeft alignLeft borderFromLeft mob50 mobpaddingInBox">
                       <div class="row red">Visa + Travel Insurance</div>
                       <div class="row label fontTwnty"><span class="strike">$328</span> $228</div>
-                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="6">
+                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="6" />
                       <input class="button-large custom-button marginFromTop" value="Apply Now" name="submit_e" id="submit_e" type="submit">
                     </div>
                      </form>
                   </div>
                 </div>
               </div>
-
-
-
-
-                            <div class="visaDiv"> 
-                                <div class="row fontBold green visaDivHeading">96 Hrs</div>
-                                <div class="row fontTwnty marginFromBottom">Transit Visa</div>
-                                <div class="entryType">Single Entry</div>
-                                <div class="row paddingAll greyBG borderFromTop marginFromTop">
-                                <div class="issueDiv">Processing Time is 24 - 72 Working Hours</div>
-                                <div class="row font16 label">Stay Validity is 96 hours from the time of entry</div>
-                                <div class="row  font16 label marginFromBottom">Visa Validity 60 days from the date of issue</div>
-                                <div class="row">
-                                <form id="visaForm" name="visaForm" action="/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
-                                    <div class="column-five paddingFromRight alignFromRight borderFromRight mob50 mobpaddingInBox">
-                                    <div class="row red">Regular Visa</div>
-                                    <div class="row label fontTwnty">$ 149</div>
-                                    <input class="button-large custom-button pull_right marginFromTop" value="Apply Now" name="submit_r" id="submit_r" type="submit">                      
-                                    </div>
-                                    <div class="column-five paddingFromLeft alignLeft borderFromLeft mob50 mobpaddingInBox">
-                                    <div class="row red">Visa + Travel Insurance</div>
-                                    <div class="row label fontTwnty"><span class="strike">$328</span> $228</div>
-                                    <input type="hidden" id="visa_type_id" name="visa_type_id" value="6">
-                                    <input class="button-large custom-button marginFromTop" value="Apply Now" name="submit_e" id="submit_e" type="submit">
-                                    </div>
-                                    </form>
-                                </div>
-                                </div>
-                            </div>
                             <div class="visaDiv"> 
                 <div class="row fontBold green visaDivHeading">14</div>
                 <div class="row fontTwnty marginFromBottom">Days Visa</div>
@@ -79,7 +188,7 @@
                   <div class="row font16 label">Stay Validity is 14 days from the date of entry</div>
                   <div class="row  font16 label marginFromBottom">Visa Validity 60 days from the date of issue</div>
                   <div class="row">
-                   <form id="visaForm" name="visaForm" action="/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
+                   <form id="visaForm" name="visaForm" action="https://www.instadubaivisa.com/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
                     <div class="column-five paddingFromRight alignFromRight borderFromRight mob50 mobpaddingInBox">
                       <div class="row red">Regular Visa</div>
                       <div class="row label fontTwnty">$ 189</div>
@@ -88,7 +197,7 @@
                     <div class="column-five paddingFromLeft alignLeft borderFromLeft mob50 mobpaddingInBox">
                       <div class="row red">Visa + Travel Insurance</div>
                       <div class="row label fontTwnty"><span class="strike">$388</span> $288</div>
-                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="1">
+                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="1" />
                       <input class="button-large custom-button marginFromTop" value="Apply Now" name="submit_e" id="submit_e" type="submit">
                     </div>
                      </form>
@@ -104,7 +213,7 @@
                   <div class="row font16 label">Stay Validity is 30 days from the date of entry</div>
                   <div class="row  font16 label marginFromBottom">Visa Validity 60 days from the date of issue</div>
                   <div class="row">
-                   <form id="visaForm" name="visaForm" action="/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
+                   <form id="visaForm" name="visaForm" action="https://www.instadubaivisa.com/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
                     <div class="column-five paddingFromRight alignFromRight borderFromRight mob50 mobpaddingInBox">
                       <div class="row red">Regular Visa</div>
                       <div class="row label fontTwnty">$ 199</div>
@@ -113,7 +222,7 @@
                     <div class="column-five paddingFromLeft alignLeft borderFromLeft mob50 mobpaddingInBox">
                       <div class="row red">Visa + Travel Insurance</div>
                       <div class="row label fontTwnty"><span class="strike">$418</span> $318</div>
-                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="2">
+                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="2" />
                       <input class="button-large custom-button marginFromTop" value="Apply Now" name="submit_e" id="submit_e" type="submit">
                     </div>
                      </form>
@@ -129,7 +238,7 @@
                   <div class="row font16 label">Stay Validity is 60 days from the date of entry</div>
                   <div class="row  font16 label marginFromBottom">Visa Validity 60 days from the date of issue</div>
                   <div class="row">
-                   <form id="visaForm" name="visaForm" action="/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
+                   <form id="visaForm" name="visaForm" action="https://www.instadubaivisa.com/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
                     <div class="column-five paddingFromRight alignFromRight borderFromRight mob50 mobpaddingInBox">
                       <div class="row red">Regular Visa</div>
                       <div class="row label fontTwnty">$ 349</div>
@@ -138,7 +247,7 @@
                     <div class="column-five paddingFromLeft alignLeft borderFromLeft mob50 mobpaddingInBox">
                       <div class="row red">Visa + Travel Insurance</div>
                       <div class="row label fontTwnty"><span class="strike">$618</span> $518</div>
-                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="7">
+                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="7" />
                       <input class="button-large custom-button marginFromTop" value="Apply Now" name="submit_e" id="submit_e" type="submit">
                     </div>
                      </form>
@@ -154,7 +263,7 @@
                   <div class="row font16 label">Stay Validity is 14 days from the date of entry</div>
                   <div class="row  font16 label marginFromBottom">Visa Validity 60 days from the date of issue</div>
                   <div class="row">
-                   <form id="visaForm" name="visaForm" action="/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
+                   <form id="visaForm" name="visaForm" action="https://www.instadubaivisa.com/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
                     <div class="column-five paddingFromRight alignFromRight borderFromRight mob50 mobpaddingInBox">
                       <div class="row red">Regular Visa</div>
                       <div class="row label fontTwnty">$ 390</div>
@@ -163,7 +272,7 @@
                     <div class="column-five paddingFromLeft alignLeft borderFromLeft mob50 mobpaddingInBox">
                       <div class="row red">Visa + Travel Insurance</div>
                       <div class="row label fontTwnty"><span class="strike">$609</span> $509</div>
-                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="21">
+                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="21" />
                       <input class="button-large custom-button marginFromTop" value="Apply Now" name="submit_e" id="submit_e" type="submit">
                     </div>
                      </form>
@@ -179,7 +288,7 @@
                   <div class="row font16 label">Stay Validity is 30 days from the date of first entry</div>
                   <div class="row  font16 label marginFromBottom">Visa Validity 60 days from the date of issue</div>
                   <div class="row">
-                   <form id="visaForm" name="visaForm" action="/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
+                   <form id="visaForm" name="visaForm" action="https://www.instadubaivisa.com/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
                     <div class="column-five paddingFromRight alignFromRight borderFromRight mob50 mobpaddingInBox">
                       <div class="row red">Regular Visa</div>
                       <div class="row label fontTwnty">$ 445</div>
@@ -188,7 +297,7 @@
                     <div class="column-five paddingFromLeft alignLeft borderFromLeft mob50 mobpaddingInBox">
                       <div class="row red">Visa + Travel Insurance</div>
                       <div class="row label fontTwnty"><span class="strike">$664</span> $564</div>
-                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="3">
+                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="3" />
                       <input class="button-large custom-button marginFromTop" value="Apply Now" name="submit_e" id="submit_e" type="submit">
                     </div>
                      </form>
@@ -204,7 +313,7 @@
                   <div class="row font16 label">Stay Validity is 60 days from the date of first entry</div>
                   <div class="row  font16 label marginFromBottom">Visa Validity 60 days from the date of issue</div>
                   <div class="row">
-                   <form id="visaForm" name="visaForm" action="/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
+                   <form id="visaForm" name="visaForm" action="https://www.instadubaivisa.com/visa_application_landing_form.php" method="post" enctype="multipart/form-data">
                     <div class="column-five paddingFromRight alignFromRight borderFromRight mob50 mobpaddingInBox">
                       <div class="row red">Regular Visa</div>
                       <div class="row label fontTwnty">$ 649</div>
@@ -213,7 +322,7 @@
                     <div class="column-five paddingFromLeft alignLeft borderFromLeft mob50 mobpaddingInBox">
                       <div class="row red">Visa + Travel Insurance</div>
                       <div class="row label fontTwnty"><span class="strike">$893</span> $793</div>
-                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="22">
+                      <input type="hidden" id="visa_type_id" name="visa_type_id" value="22" />
                       <input class="button-large custom-button marginFromTop" value="Apply Now" name="submit_e" id="submit_e" type="submit">
                     </div>
                      </form>
@@ -231,7 +340,7 @@
 <div class="row content">              
 <p class="row marginFromBottom">The only portal with the fastest <strong>online Dubai visa application </strong> processing and the highest online visa approval rate. Take a look at the <strong>Dubai visa price</strong> below. </p>  
 
-<p class="row marginFromBottom"><strong>Important Note:</strong> With Dubai visa, you gain access to all the seven emirates in the United Arab Emirates: Abu Dhabi, Ajman, Dubai, Fujairah, Sharjah, Ras al-Khaimah &amp; Umm al-Quwain. </p>  
+<p class="row marginFromBottom"><strong>Important Note:</strong> With Dubai visa, you gain access to all the seven emirates in the United Arab Emirates: Abu Dhabi, Ajman, Dubai, Fujairah, Sharjah, Ras al-Khaimah & Umm al-Quwain. </p>  
 
 <p class="row marginFromBottom">The United Arab Emirates has a lucrative visa process for its patrons to visit and enjoy the country and its landscapes. The Immigration Department has laid down clear and specific rules for Dubai visa and <strong>Dubai Visa cost</strong>. The UAE rolls out a variety of visas that match the visitor's purpose to travel. </p>  
 
@@ -268,5 +377,10 @@
     </section>
     </div>
   </div>
+  
+
+
+
+
 
 <?php include_once "include/footer.php";?>
