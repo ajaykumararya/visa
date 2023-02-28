a0e859da5701df8fa4b2d997e9e83628f002591c
Saurabh Kumar Arya <saurabhfzd000@gmail.com>
2023-02-28 14:11:32 +0530

    img

--- application/views/web/include/footer.php
+++ application/views/web/include/footer.php
@@ -6,7 +6,7 @@
 <div class="container">
 
 <div class="footerLogo">
-    <div class="secure-box" style="left:-105px;"><img class="lazy" data-src="<?=base_url()?>assets/website/media/images/payemt2.jpg" width="100" height="79" title="Secure Transaction" alt="Secure Transaction" class="lazyload"/></div>
+    <div class="secure-box" style="left:-105px;"><img class="lazy" data-src="<?=base_url()?>assets/website/media/images/payment2.jpg" width="100" height="79" title="Secure Transaction" alt="Secure Transaction" class="lazyload"/></div>
 <img  data-src="<?=base_url()?>assets/website/media/images/logo.png" width="140" height="140" class="circle lazy" alt="Insta Dubai Visa" title="Insta Dubai Visa"/>
 <div class="secure-box" style="right:-105px;"><img class="lazyload lazy" data-src="<?=base_url()?>assets/website/media/images/payment.jpg" width="100" height="79" title="Secure Payment" alt="Secure Payment"/></div>
 </div>
